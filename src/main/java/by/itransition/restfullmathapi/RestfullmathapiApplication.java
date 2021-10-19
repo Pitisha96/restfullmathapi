318523a1d4b5545cb62ddb5e1df8e926f14ede6b
Pitisha96 <petrikin1996@gmail.com>
2021-10-19 19:16:00 +0300

    Shared project

---
+++ src/main/java/by/itransition/restfullmathapi/RestfullmathapiApplication.java
@@ -0,0 +1,13 @@
+package by.itransition.restfullmathapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfullmathapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfullmathapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/itransition/restfullmathapi/RestfullmathapiApplicationTests.java
@@ -0,0 +1,13 @@
+package by.itransition.restfullmathapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfullmathapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
